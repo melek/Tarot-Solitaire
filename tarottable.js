7a55cdbdfe2492fc643a786a0076cec492e5bd7f
Lionel <melekcrescent@yahoo.com>
2018-03-11 01:13:47 -0700

    Style updates

--- tarottable.js
+++ tarottable.js
@@ -5,18 +5,18 @@ var tarottable = {};
 tarottable.init = function () { 
     document.writeln("\
         <table id='tarottable_container'>\
-        <tr><td id='tarottable_stock'></td>\
-        <td id='tarottable_discard'></td>\
-        <td id='tarottable_gap' colspan=2>\
-        <td id='tarottable_foundation_trumps'></td>\
-        <td id='tarottable_foundation_wands'></td>\
-        <td id='tarottable_foundation_cups'></td>\
-        <td id='tarottable_foundation_swords'></td>\
-        <td id='tarottable_foundation_pentacles'></td>\
+        <tr><td id='tarottable_stock' class='tarottable_cell'></td>\
+        <td id='tarottable_discard' class='tarottable_cell'></td>\
+        <td id='tarottable_gap' class='tarottable_cell' colspan=2>\
+        <td id='tarottable_foundation_trumps' class='tarottable_cell'></td>\
+        <td id='tarottable_foundation_wands' class='tarottable_cell'></td>\
+        <td id='tarottable_foundation_cups' class='tarottable_cell'></td>\
+        <td id='tarottable_foundation_swords' class='tarottable_cell'></td>\
+        <td id='tarottable_foundation_pentacles' class='tarottable_cell'></td>\
         </tr>");
         for(var c = 0; c < tarotsol.columns.length; c++)
         {
-            document.write("<td id='tarottable_col_"+c+"'></td>");
+            document.write("<td id='tarottable_col_"+c+"' class='tarottable_cell'></td>");
         }
         document.write("</tr></table>");
 };
