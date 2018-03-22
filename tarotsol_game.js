4cc0189153ccd92f81eedef91739ee1f57b97739
Lionel <melekcrescent@yahoo.com>
2018-03-21 18:30:35 -0600

    Basic functionality for selecting and moving cards

--- tarotsol_game.js
+++ tarotsol_game.js
@@ -1,3 +1,26 @@
-tarotsol.game = {
+tarotsol.game = function () {
+    active_card = false;
 
+};
+
+tarotsol.game.move_col2col = function (source_col, sub_index, dest_col) {
+    var dest_len = tarotsol.columns[dest_col].length - 1;
+    
+    // Splice out the selected cards
+    var active_pile = tarotsol.columns[source_col].splice(sub_index);
+    
+    // Iterate through the active pile and properly assign
+    for (var card = 0; card < active_pile.length; card++) {
+        tarotsol.key[active_pile[card]].pos = dest_col;
+        tarotsol.key[active_pile[card]].posi = tarotsol.columns[dest_col].length + card;
+    }
+    
+    // Add selected cards to destination column
+    tarotsol.columns[dest_col] = tarotsol.columns[dest_col].concat(active_pile)
+
+    // Unless this is the last card in the pile, ensure the next card is revealed
+    if(tarotsol.columns[source_col].length > 0) {
+        var source_len = tarotsol.columns[source_col].length - 1;
+        tarotsol.key[tarotsol.columns[source_col][source_len]].revealed = true;
+    }
 };
--- tarottable.js
+++ tarottable.js
@@ -2,12 +2,14 @@
 
 var tarottable = {
     initialized : false,
-    divs : []
+    selected : -1, // -1 is the magic number to mean nothing is selected.
+    divs : [],
+    click_events : []
 };
 
 tarottable.card_id = function (card_index) {
     return "tarotsol_card_" + tarotsol.key[card_index].pos + "_" + card_index;
-}
+};
 
 tarottable.card_div = function (card_index, stack_direction = "none") {
     // Strings to construct classes defined in project stylesheet   
@@ -16,9 +18,18 @@ tarottable.card_div = function (card_index, stack_direction = "none") {
     var stack_class_suffix_horizontal = "h";
     var stack_class_suffix_vertical = "v";
     
-    // Fallback value 
+    // Fallback values
     var stack_class = "";
     var stack_html_suffix = "";
+    
+    // If this card is selected, add that class to it's div
+    if (tarottable.selected == card_index)
+    {
+        var selected_class = " tarotsol_selected";
+    }
+    else {
+        var selected_class = "";
+    }
 
     // Construct correct stack classes if stack_direction is set to vertical|v or horizontal|h
     if (stack_direction.toLowerCase() == "vertical" || stack_direction.toLowerCase() == "v") {
@@ -32,7 +43,7 @@ tarottable.card_div = function (card_index, stack_direction = "none") {
     // If the card has been revealed, display the front card with a value. Else, display blank card back.
     if (tarotsol.key[card_index].revealed == true) {
         tarottable.divs.push(card_index);
-        return "<div id='"+tarottable.card_id(card_index)+"' class='tarotsol_card tarotsol_cardfront"+stack_class+"' >"+tarotsol.key[card_index].abbr+"</div>"+stack_html_suffix;
+        return "<div id='"+tarottable.card_id(card_index)+"' class='tarotsol_card tarotsol_cardfront"+selected_class+stack_class+"' >"+tarotsol.key[card_index].abbr+"</div>"+stack_html_suffix;
     }
     else {
         return "<div class='tarotsol_card tarotsol_cardback"+stack_class+"'></div>"+stack_html_suffix;
@@ -45,20 +56,25 @@ tarottable.card_placeholder_div = function (div_id) {
         return "<div id='"+div_id+"' class='tarotsol_card tarotsol_emptystack'></div>";
     }
     return "<div class='tarotsol_card tarotsol_emptystack'></div>";
-}
+};
 
 // Function to draw the columns
 tarottable.draw_cols = function () {
-    var theseCards = [];
-    for(var i = 0; i < tarotsol.columns.length; i++)
+    var these_cards = [];
+    for (var i = 0; i < tarotsol.columns.length; i++)
     {
-        theseCards = [];
+        these_cards = [];
         col_length = tarotsol.columns[i].length;
-        for(var j = 0; j < col_length; j++)
-        {
-            theseCards.push(tarottable.card_div(tarotsol.columns[i][j], "v"));
+        if (col_length == 0) {
+            these_cards.push(tarottable.card_placeholder_div());
         }
-        document.getElementById("tarottable_col_"+i).innerHTML = theseCards.join("");
+        else {
+            for (var j = 0; j < col_length; j++)
+            {
+                these_cards.push(tarottable.card_div(tarotsol.columns[i][j], "v"));
+            }
+        }
+        document.getElementById("tarottable_col_"+i).innerHTML = these_cards.join("");
     }
 };
 
@@ -108,13 +124,52 @@ tarottable.card_onclick = function (i) {
     console.log("Clicked " + tarotsol.key[i].name + ". Position: " + tarotsol.key[i].pos);
 };
 
+// Card selection function
+tarottable.event_select = function (card_index) {
+    if (tarottable.selected >= 0) {
+        tarotsol.game.move_col2col(tarotsol.key[tarottable.selected].pos,tarotsol.key[tarottable.selected].posi,tarotsol.key[card_index].pos);
+        console.log("Card moved and deselected!");
+        tarottable.selected = -1;
+    }
+    else {
+        tarottable.selected = card_index;
+        console.log("Selected: " + card_index + ": " + tarotsol.key[card_index].pos + "[" + tarotsol.key[card_index].posi + "]");
+    }
+    tarottable.draw();
+};
+
+// Functions to iterate through event divs and add/clear ALL events
+tarottable.add_all_events = function (card_index) {
+    for (var i = 0; i < tarottable.divs.length; i++) {
+        tarottable.add_event(tarottable.divs[i]);
+    }    
+};
+
+tarottable.clear_all_events = function (card_index) {
+    for (var i = 0; i < tarottable.divs.length; i++) {
+        tarottable.clear_event(tarottable.divs[i]);
+    }    
+    // Reset array of event divs for next run through
+    tarottable.divs = [];
+};
+
+// Helper functions to clear/add events
 tarottable.add_event = function (card_index) {
     element = document.getElementById(tarottable.card_id(card_index));
     if (element) {
-        element.addEventListener("click", function () { tarottable.card_onclick(card_index, tarotsol.key[card_index].pos) } );
+        tarottable.click_events[card_index] = function () { tarottable.event_select(card_index); };
+        element.addEventListener("click", tarottable.click_events[card_index]);
     }
 };
 
+tarottable.clear_event = function (card_index) {
+    element = document.getElementById(tarottable.card_id(card_index));
+    if (element) {
+        element.removeEventListener("click", tarottable.click_events[card_index]);
+    }
+};
+
+// Initialization function. Only run once.
 tarottable.init = function () {
     if (tarottable.initialized) return;
     document.writeln("\
@@ -139,14 +194,15 @@ tarottable.init = function () {
 // Combined drawing function
 tarottable.draw = function () {
     tarottable.init();
-    tarottable.divs = [];
+
+    // Clear events 
+    tarottable.clear_all_events(tarottable.divs);
+    
     tarottable.draw_cols();
     tarottable.draw_stock();
     tarottable.draw_waste(tarotsol.num_discard);
     tarottable.draw_foundations();
     
     // Create events
-    for (i = 0; i < tarottable.divs.length; i++) {
-        tarottable.add_event(tarottable.divs[i]);
-    }
+    tarottable.add_all_events();
 };
